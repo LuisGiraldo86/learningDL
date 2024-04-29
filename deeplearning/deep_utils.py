522103207f630b843745a0ec857b538748c7d2c9
luisggon <luisggon@flyde.io>
2024-04-29 15:49:58 +0200

    function reuter four layers model

--- deeplearning/deep_utils.py
+++ deeplearning/deep_utils.py
@@ -87,4 +87,31 @@ def reuters_model_three_layers(units:list, activ_func:str, X_train, y_train, X_v
     
     return model, history
 
+def reuters_model_four_layers(units:list, activ_func:str, X_train, y_train, X_val, y_val)->tuple:
+
+    # model definition
+    model = keras.Sequential([
+        layers.Dense(units=units[0], activation=activ_func),
+        layers.Dense(units=units[1], activation=activ_func),
+        layers.Dense(units=units[2], activation=activ_func),
+        layers.Dense(units=units[3], activation="softmax")
+    ])
+
+    # compiling the model
+    model.compile(
+        optimizer="rmsprop",
+        loss     ="categorical_crossentropy",
+        metrics  =["accuracy"]
+    )
+
+    # training the model
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=9,
+        batch_size=512,
+        validation_data=(X_val, y_val)
+    )
+    
+    return model, history
 
