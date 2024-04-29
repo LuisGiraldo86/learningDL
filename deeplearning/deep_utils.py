4cfc6703fd69f355a30ba79ec01fb500d8f847d4
luisggon <luisggon@flyde.io>
2024-04-29 15:15:13 +0200

    function to train multiclass model

--- deeplearning/deep_utils.py
+++ deeplearning/deep_utils.py
@@ -8,18 +8,21 @@ from keras import layers
 
 def imdb_model_setup(units_layer1:int, units_layer2:int, activ_func:str, X_train, y_train, X_val, y_val)->tuple:
 
+    # model definition
     model = keras.Sequential([
                   layers.Dense(units_layer1, activation=activ_func),
                   layers.Dense(units_layer2, activation=activ_func),
                   layers.Dense(1, activation="sigmoid")
               ])
 
+    # compiling the model
     model.compile(
           optimizer="rmsprop",
           loss="binary_crossentropy",
           metrics=["accuracy"]
       )
 
+    # training the model
     history = model.fit(
                     X_train,
                     y_train,
@@ -32,17 +35,20 @@ def imdb_model_setup(units_layer1:int, units_layer2:int, activ_func:str, X_train
 
 def imdb_model_setup_one_layer(units_layer:int, activ_func:str, X_train, y_train, X_val, y_val)->tuple:
 
+    # model definition
     model = keras.Sequential([
                   layers.Dense(units_layer, activation=activ_func),
                   layers.Dense(1, activation="sigmoid")
               ])
 
+    # compiling the model
     model.compile(
           optimizer="rmsprop",
           loss="binary_crossentropy",
           metrics=["accuracy"]
       )
 
+    # training the model
     history = model.fit(
                     X_train,
                     y_train,
@@ -52,3 +58,33 @@ def imdb_model_setup_one_layer(units_layer:int, activ_func:str, X_train, y_train
                 )
     
     return model, history
+
+
+def reuters_model_three_layers(units:list, activ_func:str, X_train, y_train, X_val, y_val)->tuple:
+
+    # model definition
+    model = keras.Sequential([
+        layers.Dense(units=units[0], activation=activ_func),
+        layers.Dense(units=units[1], activation=activ_func),
+        layers.Dense(units=units[2], activation="softmax")
+    ])
+
+    # compiling the model
+    model.compile(
+        optimizer="rmsprop",
+        loss     ="categorical_crossentropy",
+        metrics  =["accuracy"]
+    )
+
+    # training the model
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=9,
+        batch_size=512,
+        validation_data=(X_val, y_val)
+    )
+    
+    return model, history
+
+
