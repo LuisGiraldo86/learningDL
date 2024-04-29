f18deb9c3657b4a7a8703febed182b45987cbf64
luisggon <luisggon@flyde.io>
2024-04-29 12:53:42 +0200

    funtion setup one layer model

--- deeplearning/deep_utils.py
+++ deeplearning/deep_utils.py
@@ -29,3 +29,26 @@ def imdb_model_setup(units_layer1:int, units_layer2:int, activ_func:str, X_train
                 )
     
     return model, history
+
+def imdb_model_setup_one_layer(units_layer:int, activ_func:str, X_train, y_train, X_val, y_val)->tuple:
+
+    model = keras.Sequential([
+                  layers.Dense(units_layer, activation=activ_func),
+                  layers.Dense(1, activation="sigmoid")
+              ])
+
+    model.compile(
+          optimizer="rmsprop",
+          loss="binary_crossentropy",
+          metrics=["accuracy"]
+      )
+
+    history = model.fit(
+                    X_train,
+                    y_train,
+                    epochs=4,
+                    batch_size=512,
+                    validation_data=(X_val, y_val)
+                )
+    
+    return model, history
